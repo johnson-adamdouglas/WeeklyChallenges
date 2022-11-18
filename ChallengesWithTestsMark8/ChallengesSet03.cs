a2d7dc5d8115a7709c88b9480b8346d4a0b4f0e2
Adam Johnson <johnson.adamdouglas@gmail.com>
2022-11-18 11:05:52 -0500

    week 3 complete

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -20,36 +20,40 @@ namespace ChallengesWithTestsMark8
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            int digitCounter = 0;
-            int upperCounter = 0;
-            int lowerCounter = 0;
-            foreach (var c in password)
-            {
-                if (char.IsUpper(c))
-                {
-                    upperCounter++;
-                }
-                if (char.IsLower(c))
-                {
-                    lowerCounter++;
-                }
-                if (char.IsDigit(c))
-                {
-                    digitCounter++;
-                }
-            }
-            return upperCounter > 0 && lowerCounter > 0 && digitCounter > 0;
+            //int digitCounter = 0;
+            //int upperCounter = 0;
+            //int lowerCounter = 0;
+            //foreach (var c in password)
+            //{
+            //    if (char.IsUpper(c))
+            //    {
+            //        upperCounter++;
+            //    }
+            //    if (char.IsLower(c))
+            //    {
+            //        lowerCounter++;
+            //    }
+            //    if (char.IsDigit(c))
+            //    {
+            //        digitCounter++;
+            //    }
+            //}
+            //return upperCounter > 0 && lowerCounter > 0 && digitCounter > 0;
+
+            return password.Any(char.IsLower) && password.Any(char.IsUpper) && password.Any(char.IsDigit); 
             //throw new NotImplementedException();
         }
 
         public char GetFirstLetterOfString(string val) => val[0];
 
         //return val[0];
+        //return val.Last();  LINQ
         //throw new NotImplementedException();
 
 
         public char GetLastLetterOfString(string val) => val[val.Length - 1];
-
+        //return val.Last(); LINQ
+        //return val[^1]; carrot reverses indexes
         //throw new NotImplementedException();
 
 
@@ -66,31 +70,32 @@ namespace ChallengesWithTestsMark8
 
         public int LastMinusFirst(int[] nums)
         {
-            return nums[nums.Length - 1] - nums[0];
+            return nums[^1] - nums[0];
             //throw new NotImplementedException();
         }
 
         public int[] GetOddsBelow100() 
         {
-            int[] odds = new int[50];
-            for (int i = 0; i <= 100; i++)
-            {
-                if (i % 2 != 0)
-                {
-                    odds[i] = i;
-                }
-            }
-            return odds;
+            //var list = new List<int>();
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        list.Add(i);
+            //    }
+            //}
+            //return list.ToArray();
+            return Enumerable.Range(0, 100).Where(x => x % 2 != 0).ToArray(); //LINQ
             //throw new NotImplementedException();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            foreach (string word in words)
+            for (int i = 0; i < words.Length; i++)
             {
-                return string.ToUpper(word);
+                words[i] = words[i].ToUpper();
             }
-            
+
             //throw new NotImplementedException();
         }
     }
